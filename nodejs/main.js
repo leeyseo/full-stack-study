fb89782e44873b0a1b9a06d93c10352fffa99a32
leeyseo <102642628+leeyseo@users.noreply.github.com>
2024-09-08 01:19:01 +0900

    CRUD

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
- 
+var qs = require('querystring');
+
 
 function templateList(filelist){
   var list = '<ul>';
@@ -28,6 +29,7 @@ function templateHtml(title,list,description){
       <ul>
         ${list}
       </ul>
+      <a href="/create">create</a>
       <h2>${title}</h2>
       <p>${description}</p>
     </body>
@@ -37,11 +39,24 @@ function templateHtml(title,list,description){
 }
 
 
+var htmlForm=`
+          <form action="http://localhost:3000/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `;
+
 
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var pathname = url.parse(_url, true).pathname;
     var title = queryData.id;
     
     
@@ -51,13 +66,37 @@ var app = http.createServer(function(request,response){
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
-    response.writeHead(200);
+    if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'WEB - create';
+        var list = templateList(filelist);
+        var template = templateHtml(title, list, htmlForm);
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+          body = body + data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
+      });
+    } 
+
     fs.readdir('./data', function(error, filelist){
       fs.readFile(`data/${title}`, 'utf8', function(err, description){
         var list = templateList(filelist);
         var template=templateHtml(title,list ,description);
         response.end(template);
       });
+
     });
 });
 app.listen(3000);
